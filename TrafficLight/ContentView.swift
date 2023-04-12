0ed30595eb302bd701f13033ac324bad12983ec3
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2023-04-12 15:13:22 +0300

    feat: add models of colored circles and buttons to the view feat: implement the logic of switching the traffic light and changing the title of the button by clicking on it

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State private var buttonTitle = "START"
+    
+    @State private var redLightState = 0.3
+    @State private var yellowLightState = 0.3
+    @State private var greenLightState = 0.3
+    
+    @State private var currentLight = CurrentLight.red
+    
+    private func nextColor() {
+        let lightIsOn = 1.0
+        let lightIsOff = 0.3
+        
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            greenLightState = lightIsOff
+            redLightState = lightIsOn
+        case .yellow:
+            currentLight = .green
+            redLightState = lightIsOff
+            yellowLightState = lightIsOn
+        case .green:
+            currentLight = .red
+            greenLightState = lightIsOn
+            yellowLightState = lightIsOff
         }
-        .padding()
+    }
+}
+
+extension ContentView {
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                ColorCircleView(color: .red, opacity: redLightState)
+                ColorCircleView(color: .yellow, opacity: yellowLightState)
+                ColorCircleView(color: .green, opacity: greenLightState)
+                
+                Spacer()
+                
+                StartButtonView(title: buttonTitle) {
+                    if buttonTitle == "START" {
+                        buttonTitle = "NEXT"
+                    }
+                    nextColor()
+                }
+            }
+            .padding()
+        }
     }
 }
 
