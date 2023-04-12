f746fc833ebd421d3f2a91f02a8e18dd4b513a0e
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2023-04-12 09:30:37 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by admin on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
